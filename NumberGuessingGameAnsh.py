0f0783505bdd71c4b09e86e549a9380b59f594a2
Ansh Singh <64472289+ANSH-SINGH-2006@users.noreply.github.com>
2021-05-20 17:14:35 +0530

    Update NumberGuessingGameAnsh.py

--- NumberGuessingGameAnsh.py
+++ NumberGuessingGameAnsh.py
@@ -5,20 +5,21 @@ chance=int(5)
 print("Number Guessing Game!!")
 print("Guess a number (between 1-9): ")
 
-x=int(input("Enter your guess: "))
+
 
 while chance>0:
+    x=int(input("Enter your guess: "))
     if(x>number):
         chance=chance-1
         print("Your guess is too high, guess a number lesser than "+str(x))
         print("Chances Left: "+str(chance))
-        x=int(input("Enter your guess: "))
+        
         
     elif(x<number):
         chance=chance-1
         print("Your guess is too low, guess a number greater than "+str(x))
         print("Chances Left: "+str(chance))
-        x=int(input("Enter your guess: "))
+        
         
     else:
         print("You Won!!!")
